a9e0eb91fcfe66c3be08a9742f6d531872f78c33
Lee <lsmin476@naver.com>
2024-02-06 23:56:13 +0900

    |SM UPDATE| 유저 수정을 위한 DTO 생성 및 기능 추가

--- src/main/java/com/capstone/planet/Bean/Small/UpdateUserDAOBean.java
+++ src/main/java/com/capstone/planet/Bean/Small/UpdateUserDAOBean.java
@@ -1,2 +1,21 @@
-package com.capstone.planet.Bean.Small;public class UpdateUserDAOBean {
+package com.capstone.planet.Bean.Small;
+
+import com.capstone.planet.Model.DAO.UserDAO;
+import com.capstone.planet.Model.DTO.RequestUserUpdateDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UpdateUserDAOBean {
+
+    // 유저 정보 수정
+    public UserDAO exec(UserDAO userDAO, RequestUserUpdateDTO requestUserUpdateDTO){
+
+        userDAO.setUserId(requestUserUpdateDTO.getUserId());
+        userDAO.setPasswd(requestUserUpdateDTO.getPasswd());
+        userDAO.setNickName(requestUserUpdateDTO.getNickName());
+        userDAO.setImageUrl(requestUserUpdateDTO.getImageUrl());
+        userDAO.setAddress(requestUserUpdateDTO.getAddress());
+
+        return userDAO;
+    }
 }
--- src/main/java/com/capstone/planet/Model/DTO/RequestUserUpdateDTO.java
+++ src/main/java/com/capstone/planet/Model/DTO/RequestUserUpdateDTO.java
@@ -1,4 +1,13 @@
 package com.capstone.planet.Model.DTO;
 
+import lombok.Data;
+
+@Data
 public class RequestUserUpdateDTO {
+    Long userHandleId;
+    String userId;
+    String passwd;
+    String nickName;
+    String imageUrl;
+    String address;
 }

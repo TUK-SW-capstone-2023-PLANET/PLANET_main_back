82fce579f229d3d99ea5dc90d12a1136e6e5f155
Lee <lsmin476@naver.com>
2024-02-22 16:23:15 +0900

    |SM UPDATE| s3 이미지 저장 및 url 반환 로직

--- src/main/java/com/capstone/planet/Bean/Small/SaveImageS3Bean.java
+++ src/main/java/com/capstone/planet/Bean/Small/SaveImageS3Bean.java
@@ -1,2 +1,43 @@
-package com.capstone.planet.Bean.Small;public class SaveImageS3Bean {
+package com.capstone.planet.Bean.Small;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class SaveImageS3Bean {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    // Aws S3에 이미지 저장
+    // 이미지 url 리턴
+    // 개선가능 : String 이 아닌 클래스로 Url 리턴
+    // 개선가능 : 코드 읽기 불편한거 개선 (주석, 클래스화)
+    // 인터넷이 있는거 그냥 쓱 긁어 온거라 그냥 고치면됨
+    public String exec(MultipartFile multipartFile) throws IOException {
+        String s3FileName = "images/" + UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+
+        ObjectMetadata objMeta = new ObjectMetadata();
+        objMeta.setContentLength(multipartFile.getInputStream().available());
+
+        PutObjectResult putObjectResult = amazonS3Client.putObject(bucket, s3FileName, multipartFile.getInputStream(), objMeta);
+
+        System.out.println("putObjectResult : " + putObjectResult.toString());
+
+        return amazonS3Client.getUrl(bucket, s3FileName).toString();
+
+    }
+
 }
+

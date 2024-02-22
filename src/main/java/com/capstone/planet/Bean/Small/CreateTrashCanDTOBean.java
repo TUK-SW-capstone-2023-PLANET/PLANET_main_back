d6188f71d2890dd5f314e129272af096c27e900b
Lee <lsmin476@naver.com>
2024-02-22 18:00:48 +0900

    |SM UPDATE| Trash Can 조회 및 생성 small bean

--- src/main/java/com/capstone/planet/Bean/Small/CreateTrashCanDTOBean.java
+++ src/main/java/com/capstone/planet/Bean/Small/CreateTrashCanDTOBean.java
@@ -1,2 +1,22 @@
-package com.capstone.planet.Bean.Small;public class CreateTrashCanDTOBean {
+package com.capstone.planet.Bean.Small;
+
+import com.capstone.planet.Model.DAO.TrashCanDAO;
+import com.capstone.planet.Model.DTO.ResponseTrashCanGetDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreateTrashCanDTOBean {
+
+    // trash can DTO 생성
+    public ResponseTrashCanGetDTO exec(TrashCanDAO trashCanDAO){
+
+        ResponseTrashCanGetDTO responseTrashCanGetDTO = ResponseTrashCanGetDTO.builder()
+                .trashCanId(trashCanDAO.getTrashCanId())
+                .location(trashCanDAO.getLatitude(), trashCanDAO.getLongitude())
+                .address(trashCanDAO.getAddress())
+                .imageUrl(trashCanDAO.getImageUrl())
+                .build();
+
+        return responseTrashCanGetDTO;
+    }
 }
--- src/main/java/com/capstone/planet/Bean/Small/GetTrashCanDAOBean.java
+++ src/main/java/com/capstone/planet/Bean/Small/GetTrashCanDAOBean.java
@@ -1,2 +1,22 @@
-package com.capstone.planet.Bean.Small;public class GetTrashCanDAOBean {
+package com.capstone.planet.Bean.Small;
+
+import com.capstone.planet.Model.DAO.TrashCanDAO;
+import com.capstone.planet.Repository.TrashCanRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GetTrashCanDAOBean {
+
+    TrashCanRepositoryJPA trashCanRepositoryJPA;
+
+    @Autowired
+    public GetTrashCanDAOBean(TrashCanRepositoryJPA trashCanRepositoryJPA) {
+        this.trashCanRepositoryJPA = trashCanRepositoryJPA;
+    }
+
+    // 쓰레기통 정보 가져오기
+    public TrashCanDAO exec(Long trashCanId){
+        return trashCanRepositoryJPA.findById(trashCanId).orElse(null);
+    }
 }
